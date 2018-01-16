f7ba2681210e0f7a9722668f205f2cb6c01c36bf
nemanja-jevtovic <jevta993@gmail.com>
2018-01-16 14:22:23 +0100

    add search component

---
+++ src/components/common/search.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = this.initState();
+
+        this.bindEventHandlers();
+    }
+
+    initState() {
+        return {
+            inputString: "",
+            searchString: ""
+        };
+    }
+
+    bindEventHandlers() {
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput(event) {
+        const inputString = event.target.value;
+        const searchString = inputString.toLowerCase();
+
+        this.setState({ inputString: inputString, searchString: searchString });
+
+        this.props.onSearch(searchString);
+    }
+
+    render() {
+        const inputString = this.state.inputString;
+
+        return (
+            <div className="input-group mt-1 mb-1">
+                <div className="input-group-prepend">
+                    <span className="input-group-text">
+                        <i className="fa fa-search"></i>
+                    </span>
+                </div>
+                <input
+                    className="form-control"
+                    type="text"
+                    value={inputString}
+                    onChange={this.handleInput}
+                    placeholder="Search..."
+                />
+            </div>
+        );
+    }
+}
+
+export default Search;
