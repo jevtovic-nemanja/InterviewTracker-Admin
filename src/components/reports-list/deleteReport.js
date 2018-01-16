45cd098ae994a5bbf2090dd439cf88824099a1cf
nemanja-jevtovic <jevta993@gmail.com>
2018-01-16 22:39:20 +0100

    style delete modal buttons

--- src/components/reports-list/deleteReport.js
+++ src/components/reports-list/deleteReport.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export const DeleteReport = props => {
     return (
-        <div className="row">
+        <div className="row mt-4 pt-1">
             <div className="col-12">
-                <p>Are you sure you wish to delete this report?</p>
+                <p className="mb-4">Are you sure you wish to delete this report?</p>
                 <div className="float-right">
-                    <button type="button" className="btn" onClick={props.deleteReport}>Yes</button>
-                    <button type="button" className="btn" onClick={props.close}>No</button>
+                    <button type="button" className="btn btn-delete mr-2" onClick={props.deleteReport}>Yes</button>
+                    <button type="button" className="btn btn-close" onClick={props.close}>No</button>
                 </div>
             </div>
         </div>
