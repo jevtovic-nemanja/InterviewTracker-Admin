1c9d9ff7ac44fac21a9b2017a2a7a5121dc5c066
nemanja-jevtovic <jevta993@gmail.com>
2018-01-17 20:40:15 +0100

    style select companies component

--- src/components/create-report/wizard/companies.js
+++ src/components/create-report/wizard/companies.js
@@ -4,12 +4,12 @@ import Search from "../../common/search";
 
 export const SelectCompany = props => {
     return (
-        <div className="row">
-            <div className="col-12 offset-sm-1 col-sm-10 col-md-11 offset-lg-6 col-lg-6 offset-xl-6 col-xl-6">
+        <div className="row mt-4">
+            <div className="col-12 offset-lg-6 col-lg-6">
                 <Search onSearch={props.onSearch} />
             </div>
-            <div className="col-12 offset-sm-1 col-sm-10 offset-md-1 col-md-11">
-                <table className="table table-striped table-bordered">
+            <div className="col-12 mt-2">
+                <table className="table table-striped table-bordered table-hover">
                     <tbody>
                         {props.companies.map(company => {
                             const { id, name } = company;
