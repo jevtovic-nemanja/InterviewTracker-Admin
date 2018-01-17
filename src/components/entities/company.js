f24728203810ec223ba714478ce4d13bc068abbf
nemanja-jevtovic <jevta993@gmail.com>
2018-01-17 13:10:01 +0100

    add company dto

---
+++ src/components/entities/company.js
@@ -0,0 +1,16 @@
+class Company {
+    constructor(id, name) {
+        this._id = id;
+        this._name = name;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get name() {
+        return this._name;
+    }
+}
+
+export default Company;
