0cd4f2cb5558f31c5aa1ace9433eecc900b921f7
nemanja-jevtovic <jevta993@gmail.com>
2018-01-17 15:39:06 +0100

    create aside component

--- src/components/create-report/wizard/aside.js
+++ src/components/create-report/wizard/aside.js
@@ -3,6 +3,48 @@ import React from "react";
 export const Aside = props => {
 
     return (
-        <p>Aside</p>
+        <div className="row wizard-aside">
+            <div className="col-12">
+                <div className="bottom-line-aside">
+                    <h5 className="mt-3">
+                        <small className="fa-stack">
+                            <span className="fa fa-circle-thin fa-stack-2x"></span>
+                            <strong className="fa-stack-1x">1</strong>
+                        </small>
+                        <span className="pl-2 move-down">
+                            Select Candidate
+                        </span>
+                    </h5>
+                    <h5 className="mt-3">
+                        <small className="fa-stack">
+                            <span className="fa fa-circle-thin fa-stack-2x"></span>
+                            <strong className="fa-stack-1x">2</strong>
+                        </small>
+                        <span className="pl-2 move-down">
+                            Select Company
+                        </span>
+                    </h5>
+                    <h5 className="mt-3 mb-4">
+                        <small className="fa-stack">
+                            <span className="fa fa-circle-thin fa-stack-2x"></span>
+                            <strong className="fa-stack-1x">3</strong>
+                        </small>
+                        <span className="pl-2 move-down">
+                            Fill Report Detail
+                        </span>
+                    </h5>
+                </div>
+                <div>
+                    <div>
+                        <p className="mt-3 mb-1">Candidate:</p>
+                        <h4>Candidate Name</h4>
+                    </div>
+                    <div className="pl-1">
+                        <p className="mb-1">Company:</p>
+                        <h4>Company Name</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
