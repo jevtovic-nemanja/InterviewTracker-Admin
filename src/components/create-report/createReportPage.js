765157ebcf43374e1ab2b30d5f7d232ead7a72d0
nemanja-jevtovic <jevta993@gmail.com>
2018-01-17 19:05:28 +0100

    style search, aside and candidates components

--- src/components/create-report/createReportPage.js
+++ src/components/create-report/createReportPage.js
@@ -78,10 +78,10 @@ class CreateReportPage extends React.Component {
                 <div className="row mt-4">
                     <div className="offset-1 col-10 offset-sm-0 col-sm-12 card">
                         <div className="row card-body">
-                            <aside className="col-12 col-md-4">
+                            <aside className="col-12 offset-sm-1 col-sm-10 offset-md-0 col-md-4 col-xl-3">
                                 <Aside />
                             </aside>
-                            <main className="col-12 col-md-8">
+                            <main className="col-12 col-md-8 col-xl-9">
                                 <SelectCandidate candidates={filteredCandidates} onSearch={this.filterCandidates} />
                                 <SelectCompany />
                                 <FillReport />
--- src/components/create-report/wizard/candidates.js
+++ src/components/create-report/wizard/candidates.js
@@ -6,21 +6,21 @@ export const SelectCandidate = props => {
 
     return (
         <div className="row">
-            <div className="col-12 offset-sm-1 col-sm-10 offset-lg-5 col-lg-6">
+            <div className="col-12 offset-sm-1 col-sm-10 col-md-11 offset-lg-6 col-lg-6 offset-xl-6 col-xl-6">
                 <Search onSearch={props.onSearch} />
             </div>
 
             {props.candidates.map(candidate => {
                 const { id, name, email, avatar } = candidate;
                 return (
-                    <div key={id} className="col-12 offset-sm-1 col-sm-10">
+                    <div key={id} className="col-12 offset-sm-1 col-sm-10 offset-md-1 col-md-11 offset-xl-0 col-xl-6">
                         <div className="card candidate-card mx-auto my-2 p-2">
                             <div className="row">
                                 <div className="col-3 text-center">
                                     <img
                                         src={avatar ? avatar : "../../../assets/images/avatar.png"}
                                         alt="Candidate picture"
-                                        className="rounded-circle candidate-img w-100"
+                                        className="rounded-circle candidate-img w-50"
                                     />
                                 </div>
                                 <div className="col-9 d-flex flex-column justify-content-center">
