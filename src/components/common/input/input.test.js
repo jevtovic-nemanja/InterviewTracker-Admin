9c5ec3e9bd242be2e1126d572fd2996d8492ab4d
Nemanja Jevtovic <Nemanja.Jevtovic@endava.com>
2018-04-19 17:57:00 +0200

    add test for input component

---
+++ src/components/common/input/input.test.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({
+    adapter: new Adapter
+});
+
+import { Input } from "./input";
+
+describe("<Input />", () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<Input />);
+    });
+
+    it("should display the passed value", () => {
+        const text = "text";
+
+        wrapper.setProps({
+            inputItem: text
+        });
+
+        expect(wrapper.find("input").props().value).toEqual(text);
+    });
+
+    it("should display what the user types in", () => {
+        const text = "text";
+        const newText = "newText";
+        const mockedEvent = {
+            target: {
+                value: newText
+            }
+        };
+
+        const receiveInputChange = event => {
+            wrapper.setProps({
+                inputItem: event.target.value
+            });
+        };
+
+        wrapper.setProps({
+            inputItem: text,
+            receiveInputChange: receiveInputChange
+        });
+
+        wrapper.find("input").simulate("change", mockedEvent);
+
+        expect(wrapper.find("input").props().value).toEqual(newText);
+    })
+});
