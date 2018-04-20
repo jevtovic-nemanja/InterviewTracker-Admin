e30b562777b39c7ed4c7a3f06051356a5293b4cf
Nemanja Jevtovic <Nemanja.Jevtovic@endava.com>
2018-04-20 15:27:46 +0200

    add test for DeleteReport component

---
+++ src/components/reports-list/deleteReport/deleteReport.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({
+    adapter: new Adapter
+})
+
+import { DeleteReport } from "./deleteReport";
+
+describe("<DeleteReport />", () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<DeleteReport />);
+    });
+
+    it("displays error message if it exists", () => {
+        const text = "text";
+
+        wrapper.setProps({
+            message: text
+        });
+
+        expect(wrapper.find("p").text()).toEqual(text);
+        expect(wrapper.find("button")).toHaveLength(1);
+    });
+
+    it("displays default delete modal if there is no error message", () => {
+        wrapper.setProps({
+            message: ""
+        });
+
+        expect(wrapper.find("p").text()).toEqual("Are you sure you wish to delete this report?");
+        expect(wrapper.find("button")).toHaveLength(2);
+    })
+});
