bc95d1a0880042ded408640cebb97cf63d5c3d3d
Nemanja Jevtovic <Nemanja.Jevtovic@endava.com>
2018-05-21 17:26:07 +0200

    add test for notes component

---
+++ src/components/create-report/wizard/newReport/notes/notes.test.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({
+    adapter: new Adapter
+})
+
+import { Notes } from "./notes";
+
+const createTestProps = props => ({
+    labelText: "Notes",
+    value: "note",
+    onChange: jest.fn(),
+    declined: "",
+    ...props
+});
+
+describe("<Notes />", () => {
+
+    describe("if enabled", () => {
+        let props;
+        let wrapper;
+
+        beforeEach(() => {
+            props = createTestProps();
+            wrapper = shallow(<Notes {...props} />);
+        });
+
+        it("displays the correct elements", () => {
+            expect(wrapper.find("label")).toHaveLength(1);
+            expect(wrapper.find("textarea")).toHaveLength(1);
+            expect(wrapper.find("textarea").props().disabled).toBeFalsy();
+        });
+
+        it("displays the passed value",() => {
+            expect(wrapper.find("textarea").props().value).toEqual(props.value);
+        });
+
+        it("calls the onChange method", () => {
+            wrapper.find("textarea").simulate("change");
+            expect(props.onChange).toBeCalled();
+        });
+    });
+
+    describe("if disabled", () => {
+        const props = createTestProps({
+            declined: "disabled"
+        });
+
+        const wrapper = shallow(<Notes {...props} />);
+
+        it("displays the correct components", () => {
+            expect(wrapper.find("label")).toHaveLength(1);
+            expect(wrapper.find("textarea")).toHaveLength(1);
+            expect(wrapper.find("textarea").props().disabled).toBeTruthy();
+        });
+    });
+});
