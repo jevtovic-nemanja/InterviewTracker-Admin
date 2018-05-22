cbf5e6e8ed478bd2d4f08a1328f604d3cee5f295
Nemanja Jevtovic <Nemanja.Jevtovic@endava.com>
2018-05-22 11:33:48 +0200

    refactor packer method to use object lookup instead of switch

--- src/utils/packer.js
+++ src/utils/packer.js
@@ -4,43 +4,37 @@ import Report from "Entities/report";
 import Candidate from "Entities/candidate";
 import Company from "Entities/company";
 
-export const packer = (type, data) => {
-    switch (type) {
-    case "reports":
-        return packReports(data);
-    case "candidates":
-        return packCandidates(data);
-    case "companies":
-        return packCompanies(data);
-    }
-};
+export const packer = (responseType, responseData) => {
+    
+    const packMethods = {
+        "reports": data => {
+            const reports = data.reverse().map(item => {
+                const { id, candidateId, candidateName, companyId, companyName, interviewDate, phase, status, note } = item;
+                const date = moment(interviewDate).format("DD.MM.YYYY");
+                const reportObj = new Report(id, candidateId, candidateName, companyId, companyName, date, phase, status, note);
+                return reportObj;
+            });
+            return reports;
+        },
 
-const packReports = data => {
-    let reports = data.reverse().map(item => {
-        const { id, candidateId, candidateName, companyId, companyName, interviewDate, phase, status, note } = item;
-        const date = moment(interviewDate).format("DD.MM.YYYY");
-        const reportObj = new Report(id, candidateId, candidateName, companyId, companyName, date, phase, status, note);
-        return reportObj;
-    });
-    return reports;
-};
+        "candidates": data => {
+            const candidates = data.map(item => {
+                const { id, name, email, avatar } = item;
+                const candidateObj = new Candidate(id, name, email, avatar);
+                return candidateObj;
+            });
+            return candidates;
+        },
 
+        "companies": data => {
+            const companies = data.map(item => {
+                const { id, name } = item;
+                const companyObj = new Company(id, name);
+                return companyObj;
+            });
+            return companies;
+        }
+    };
 
-const packCandidates = data => {
-    let candidates = data.map(item => {
-        const { id, name, email, avatar } = item;
-        const candidateObj = new Candidate(id, name, email, avatar);
-        return candidateObj;
-    });
-    return candidates;
-};
-
-
-const packCompanies = data => {
-    let companies = data.map(item => {
-        const { id, name } = item;
-        const companyObj = new Company(id, name);
-        return companyObj;
-    });
-    return companies;
+    return packMethods[responseType](responseData);
 };
