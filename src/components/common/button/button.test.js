0ad2443c1a1304351d600632cc8ca0661916fb2b
Nemanja Jevtovic <Nemanja.Jevtovic@endava.com>
2018-06-04 15:16:47 +0200

    add test for Button common component

--- src/components/common/button/button.test.js
+++ src/components/common/button/button.test.js
@@ -1,71 +1,101 @@
-// import React from "react";
+import React from "react";
 
-// import { configure, shallow, mount } from "enzyme";
-// import Adapter from "enzyme-adapter-react-16";
+import { configure, shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-// configure({
-//     adapter: new Adapter
-// })
+configure({
+    adapter: new Adapter
+})
 
-// import { NextButton } from "./nextButton";
+import { Button } from "./Button";
 
-// import { DISABLED } from "Src/constants";
+import { DISABLED } from "Src/constants";
 
-// const createTestProps = props => ({
-//     next: "",
-//     incrementPhase: jest.fn(),
-//     newLocation: "#/new",
-//     ...props
-// });
+const createTestProps = props => ({
+    isDisabled: "",
+    type: "btnBack",
+    action: jest.fn(),
+    newLocation: "#/new",
+    ...props
+});
 
-// describe("<NextButton />", () => {
+describe("<Button />", () => {
 
-//     describe("if enabled", () => {
-//         let props;
-//         let wrapper;
+    describe("always", () => {
+        const props = createTestProps();
+        const wrapper = shallow(<Button {...props} />);
 
-//         beforeEach(() => {
-//             props = createTestProps();
-//             wrapper = shallow(<NextButton {...props} />);
-//         });
+        it("displays the correct element with the correct labels", () => {
+            expect(wrapper.find("button")).toHaveLength(1);
+            expect(wrapper.find("button").text()).toEqual("Back");
 
-//         it("displays the correct elements", () => {
-//             expect(wrapper.find("button")).toHaveLength(1);
-//             expect(wrapper.find("button").text()).toEqual("Next");
-//             expect(wrapper.find("button").props().disabled).toBeFalsy();
-//             expect(wrapper.find(".disabled")).toHaveLength(0);
-//         });
+            wrapper.setProps({
+                type: "btnNext"
+            });
+            expect(wrapper.find("button").text()).toEqual("Next");
 
-//         it("calls incrementPhase and opens new location in navigator on click", () => {
-//             wrapper.find("button").simulate("click");
-//             expect(props.incrementPhase).toBeCalled();
-//             expect(location.hash).toEqual(props.newLocation);
-//         });
-//     });
+            wrapper.setProps({
+                type: "btnSubmit"
+            });
+            expect(wrapper.find("button").text()).toEqual("Submit");
+        });
+    });
 
-//     describe("if disabled", () => {
-//         let props;
-//         let wrapper;
+    describe("if enabled", () => {
+        let props;
+        let wrapper;
 
-//         beforeEach(() => {
-//             props = createTestProps({
-//                 next: DISABLED,
-//                 newLocation: "#/not-called"
-//             });
-//             wrapper = mount(<NextButton {...props} />);
-//         });
+        beforeEach(() => {
+            props = createTestProps();
+            wrapper = shallow(<Button {...props} />);
+        });
 
-//         it("displays the correct elements", () => {
-//             expect(wrapper.find("button")).toHaveLength(1);
-//             expect(wrapper.find("button").text()).toEqual("Next");
-//             expect(wrapper.find("button").props().disabled).toBeTruthy();
-//             expect(wrapper.find(".disabled")).toHaveLength(1);
-//         });
+        it("displays an enabled button", () => {
+            expect(wrapper.find("button").props().disabled).toBeFalsy();
+            expect(wrapper.find(".disabled")).toHaveLength(0);
+        });
 
-//         it("doesn't call incrementPhase nor opens a new location in navigator on click", () => {
-//             wrapper.find("button").simulate("click");
-//             expect(props.incrementPhase).not.toBeCalled();
-//             expect(location.hash).not.toEqual(props.newLocation);
-//         });
-//     })
-// });
+        it("calls the passed action and opens the new location", () => {
+            wrapper.find("button").simulate("click");
+            expect(props.action).toBeCalled();
+            expect(location.hash).toEqual(props.newLocation);
+        });
+    });
+
+    describe("if disabled", () => {
+        let props;
+        let wrapper;
+
+        beforeEach(() => {
+            props = createTestProps({
+                isDisabled: DISABLED,
+                newLocation: "#/notCalled"
+            });
+            wrapper = mount(<Button {...props} />);
+        });
+
+        it("displays a disabled button", () => {
+            expect(wrapper.find("button").props().disabled).toBeTruthy();
+            expect(wrapper.find(".disabled")).toHaveLength(1);
+        });
+
+        it("doesn't call the passed action nor opens the new location", () => {
+            wrapper.find("button").simulate("click");
+            expect(props.action).not.toBeCalled();
+            expect(location.hash).not.toEqual(props.newLocation);
+        });
+    })
+
+    describe("if no new location is passed", () => {
+        const props = createTestProps({
+            newLocation: ""
+        });
+        const wrapper = shallow(<Button {...props} />);
+
+        it("calls the passed action, but doesn't open the new location", () => {
+            wrapper.find("button").simulate("click");
+            expect(props.action).toBeCalled();
+            expect(location.hash).not.toEqual(props.newLocation);
+        });
+    });
+});
